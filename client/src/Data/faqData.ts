d3aad0d46de5b4d9fdd8814376ae7f7a952f9cb7
new mac <newmac@news-mbp.lan>
2023-08-30 11:25:46 -0400

    Sets up initial data and configs

---
+++ client/src/Data/faqData.ts
@@ -0,0 +1,53 @@
+export const faqData =
+[
+  {
+    "id": 1,
+    "question": "How do I reset my password?",
+    "answer": "To reset your password, go to the login page and click the 'Forgot Password' link. Follow the prompts to enter your email address and you'll receive a link to reset your password."
+  },
+  {
+    "id": 2,
+    "question": "What is your return policy?",
+    "answer": "Our return policy allows for returns within 30 days of purchase. The product must be in its original condition and packaging. Refunds will be processed within 7 business days."
+  },
+  {
+    "id": 3,
+    "question": "How do I track my order?",
+    "answer": "After placing an order, you'll receive a confirmation email with a tracking number. You can use this number on our website to track the status of your order."
+  },
+  {
+    "id": 4,
+    "question": "Do you offer international shipping?",
+    "answer": "Yes, we do offer international shipping. Additional charges and longer delivery times may apply."
+  },
+  {
+    "id": 5,
+    "question": "How do I cancel my subscription?",
+    "answer": "To cancel your subscription, go to your account settings and click on 'Manage Subscription.' From there, you can choose to cancel your subscription."
+  },
+  {
+    "id": 6,
+    "question": "Is my data secure?",
+    "answer": "We take data security seriously. All transactions are encrypted and we follow industry best practices to protect your personal information."
+  },
+  {
+    "id": 7,
+    "question": "How do I update my payment information?",
+    "answer": "To update your payment information, go to 'Account Settings' and then click on 'Payment Methods.' You can add or update your payment details there."
+  },
+  {
+    "id": 8,
+    "question": "Do you offer any discounts?",
+    "answer": "We often have seasonal and promotional discounts. Subscribe to our newsletter to stay updated on special offers."
+  },
+  {
+    "id": 9,
+    "question": "How do I contact customer support?",
+    "answer": "You can contact customer support via email, chat, or phone. Our support hours are 9am-6pm, Monday through Friday."
+  },
+  {
+    "id": 10,
+    "question": "What is the warranty on your products?",
+    "answer": "Our products come with a one-year limited warranty that covers defects in materials and workmanship."
+  }
+]
---
+++ client/src/config/config.ts
@@ -0,0 +1,2 @@
+export const BOT_NAME = 'Jarvis'
+export const API_KEY = 'YOUR API KEY'
